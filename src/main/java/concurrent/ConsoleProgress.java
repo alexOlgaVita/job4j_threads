52e092414620ce10e780cdf41107d11395ecb2a9
olga.ilyina <olga.ilyina@axenix.pro>
2025-01-25 22:57:03 +0300

    5. Прерывание нити [#1019]

---
+++ src/main/java/concurrent/ConsoleProgress.java
@@ -0,0 +1,31 @@
+package concurrent;
+
+public class ConsoleProgress implements Runnable {
+
+    @Override
+    public void run() {
+        var process = new char[] {'-', '\\', '|', '/'};
+        int i = -1;
+        while (!Thread.currentThread().isInterrupted()) {
+            try {
+                Thread.sleep(500);
+                i = (i == 3) ? 0 : ++i;
+                System.out.print("\r load: " + process[i]);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        Thread progress = new Thread(new ConsoleProgress());
+        progress.start();
+        try {
+            Thread.sleep(5000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+            progress.interrupt();
+        }
+        progress.interrupt();
+    }
+}
