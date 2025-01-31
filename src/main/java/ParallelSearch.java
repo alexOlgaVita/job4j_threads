7061d43c0041b57e74fb3f14c3cbb5ee80084a57
olga.ilyina <olga.ilyina@axenix.pro>
2025-01-31 11:07:44 +0300

    2. Обеспечить остановку потребителя. [#66825 #530737]

--- src/main/java/ParallelSearch.java
+++ src/main/java/ParallelSearch.java
@@ -1,10 +1,10 @@
 public class ParallelSearch {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         SimpleBlockingQueue<Integer> queue = new SimpleBlockingQueue<Integer>();
         final Thread consumer = new Thread(
                 () -> {
-                    while (!Thread.interrupted()) {
+                    while (!queue.isEmpty() || !Thread.currentThread().isInterrupted()) {
                         try {
                             System.out.println(queue.poll());
                         } catch (InterruptedException e) {
@@ -15,23 +15,17 @@ public class ParallelSearch {
                 },
                 "consumer"
         );
-
         final Thread producer = new Thread(
                 () -> {
                     for (int index = 0; index != 3; index++) {
                         queue.offer(index);
-                        try {
-                            Thread.sleep(500);
-                        } catch (InterruptedException e) {
-                            e.printStackTrace();
-                        }
                     }
-                    Thread.currentThread().interrupt();
-                    consumer.interrupt();
                 },
                 "producer"
         );
         producer.start();
         consumer.start();
+        producer.join();
+        consumer.interrupt();
     }
 }
